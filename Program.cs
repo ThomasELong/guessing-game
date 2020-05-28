401990915b236d7955c9b6d937329fe5ce3caba3
ThomasELong <thomaselong12@gmail.com>
2020-05-28 09:49:42 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rand = new Random();
+
+            int secretNumber =rand.Next(1, 101);
+
+            int guessesGiven = 0;
+            Console.WriteLine("Welcome to ***Guess The Number*** Choose your difficulty level- Easy, Medium, or Hard");
+            string difficultyLevel = Console.ReadLine();
+
+
+            while(guessesGiven < 4)
+            { 
+                Console.WriteLine($"Guess the Secret Number! Your guess ({guessesGiven + 1})");
+                string userGuess = Console.ReadLine();
+                int parsedUserGuess = Int32.Parse(userGuess);
+
+                if (parsedUserGuess > secretNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (parsedUserGuess < secretNumber)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if (parsedUserGuess == secretNumber) 
+                {
+                Console.WriteLine("You Win!");
+                break;
+                }
+                else 
+                {
+                Console.WriteLine("Nope, try again!");
+                }
+
+            guessesGiven++;
+        }
+           
+        }
+    }
+}
